45d1fca07b906ed2024a7feb720191b6da994c2c
1c7 <guokrfans@gmail.com>
2018-03-23 22:49:13 +0800

    first

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,61 @@
+class TestController < ApplicationController
+  require 'rest-client'
+  require 'json'
+  require 'royalpay_service'
+  
+  def index
+    ######### SECRET ##########
+    # DO NOT LEAK, VERY IMPORTANT
+    royalpay_partner_code = ''
+    royalpay_credential_code = ''
+    ######### SECRET ##########
+    
+    order_id = 'testonly5678-1'
+    base_URL = "https://mpay.royalpay.com.au/api/v1.0/gateway/partners/#{royalpay_partner_code}/orders/#{order_id}"
+    
+    # 支付所需参数
+    payload = {
+      description: 'haha',
+      price: 1,
+      currency: 'AUD',
+      channel: 'Wechat',
+      operator: 'Operator A',
+    }
+    
+    # 生成签名
+    time = DateTime.now.strftime('%Q') 
+    nonce_str = SecureRandom.uuid.tr('-', '') # 长度 32 位
+    sign = RoyalPay::sign(time, nonce_str, royalpay_partner_code, royalpay_credential_code)
+    
+    # 把必要参数拼到 URL 里
+    params = {
+      time: time,
+      nonce_str: nonce_str,
+      sign: sign
+    }
+    final_URL = base_URL + "?" + params.map{|k,v| "#{k}=#{CGI::escape(v.to_s)}"}.join('&')
+
+    # 发请求
+    http_return = RestClient.put(final_URL, payload.to_json, content_type: :json)
+    
+    result = http_return.body
+    j = JSON.parse(result)
+    # 返回示例：
+    # {
+    # "partner_order_id": "testonly5678",
+    # "full_name": "XXXX INTERNATIONAL TRADING PTY LTD",
+    # "code_url": "https://mpay.royalpay.com.au/api/v1.0/payment/partners/XXXX/orders/0404020180321191303653XXXX/retail_pay",
+    # "partner_name": "XXXX",
+    # "channel": "Wechat",
+    # "result_code": "EXISTS",
+    # "partner_code": "XXXX",
+    # "order_id": "04040201803211913036538217",
+    # "return_code": "SUCCESS",
+    # "pay_url": "https://mpay.royalpay.com.au/api/v1.0/gateway/partners/JYYP/orders/testonly5678/pay",
+    # "qrcode_img": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAAEsAQAAAABRBrPYAAACE0lEQVR42u2aS47DIBBE2yuOwU1tfFOOMEuvYKhqcGInI83WJSwrwvBWTdE/YvU/z49NbGITm9jEFLBieLY2CLn97u09Yq0Zs0ELwzivjTl8Pq8A8taXlLD8NtlgWGMhvCpibXI5YsISLLPpYs0Oe6XIOViDJObCftvxZpz6x1l4NEbHBQtc36/+7dFYfw6Qy/DM+18x68lYoUM2i8VwnBf6rmSRalfDmssqQ9j7YZR65EAKq1yip4op9COc6KjXoIUdWE08wi0GwThd7ZetF8AK5G0ItSPs+r7fg5EAFhBqC5eayJNrHr4rX86CBFZGPpw8e+Qndj9IYZ4S0wI0Ak8xxRCLGMZkeOFBTi7sVgt8iFwAo7zhu2r13YdZPFu+xiwFzPOK1UbJM+Brbvl4rFcBqF6xxCqgugaKFgaDeNo/opKdM3LYPpIK3/SWG38JRgIYPFXPhF/ByDy/ksLojSFyt8mo3Gu6nYXnY0PkKOhSzx6NwTebGNb7h6fIaZZQ72WsAtZzDLaFz7Sq13piGPRsLnIv2PFZP/pvz8cQd14JhvfZxk2HEuaPt2JYvA+vRZsoYWVYYB1mcSutt5glgPVbG55oNks3JsYlfOmTPxzj9Q3dF9R+dIV7CS+J+fUNmzCeJJsoxr53OEWOTkUKYth5UVV5Y+735pHhSQs7L6o48BrWG6emhc2/uExsYhObmD72C5insIKPLkB/AAAAAElFTkSuQmCC"
+    # }
+
+    @qr_code = j['qrcode_img']
+  end
+end
+
---
+++ app/services/royalpay_service.rb
@@ -0,0 +1,91 @@
+# RoyalPay  https://royalpay.com.au/
+# 这里是 RoyalPay 的核心代码
+# 包括：发请求获得付款二维码（微信/支付宝），验证支付成功时 RoyalPay notify_url 的签名
+module RoyalPay
+  module_function
+
+  # 创建QRCode支付单
+  # https://mpay.royalpay.com.au/docs/cn/#api-QRCode-NewQRCode
+  def create_qr_code_payment(out_trade_no, total_fee, description, notify_url, channel)
+    # out_trade_no 商户订单号（我们自己的）
+    # total_fee    要支付多少钱， 单位 '分'
+    # description  订单标题
+    # channel 	   支付渠道，大小写敏感，允许值: Alipay, Wechat
+
+    royalpay_partner_code = 'XXXX'
+    royalpay_credential_code = 'AAAAI2M5hVSbBBBBpncrRZjjvZWEZZZZ'
+    
+    base_URL = "https://mpay.royalpay.com.au/api/v1.0/gateway/partners/#{royalpay_partner_code}/orders/#{out_trade_no}"
+    # 支付所需参数
+    payload = {
+      description: description,
+      price: total_fee,
+      currency: 'AUD',  # Allowed values: AUD, CNY
+      channel: channel, # Allowed values: Alipay, Wechat
+      notify_url: notify_url,
+    }
+    # 生成签名
+    time = DateTime.now.strftime('%Q') 
+    nonce_str = SecureRandom.uuid.tr('-', '') # 长度 32 位
+    sign = sign(time, nonce_str, royalpay_partner_code, royalpay_credential_code)
+    
+    # 把必要参数拼到 URL 里
+    params = {
+      time: time,
+      nonce_str: nonce_str,
+      sign: sign
+    }
+    final_URL = base_URL + "?" + params.map{|k,v| "#{k}=#{CGI::escape(v.to_s)}"}.join('&')
+
+    # 发请求
+    http_return = RestClient.put(final_URL, payload.to_json, content_type: :json)
+    result = http_return.body
+    json = JSON.parse(result)
+    return json
+  end
+  
+  # 生成 RoyalPay 签名
+  # 具体签名步骤文档里有：https://mpay.royalpay.com.au/docs/cn
+  def sign(time, nonce_str, royalpay_partner_code, royalpay_credential_code)
+    # time:                     当前UTC时间的毫秒数时间戳，例子：1521620326883
+    # nonce_str:                随机字符串
+    # royalpay_partner_code:    商户编码，由4位大写字母或数字构成
+    # royalpay_credential_code: 系统为商户分配的开发校验码
+    valid_string = "#{royalpay_partner_code}&#{time}&#{nonce_str}&#{royalpay_credential_code}"
+    sign = Digest::hexencode(Digest::SHA256.digest(valid_string)).downcase
+    return sign
+    # example: 8e08ce01f520761df90477f0887672a62ee9b145aec03fa35dc0954ff11950bb
+  end
+  
+  # 验证签名是否正确, 用于 RoyalPay 支付成功时的回调
+  # 返回 true / false
+  def verify_sign(time, nonce_str, sign)
+    # 验证签名的思路：
+    # 之前我们发请求给 RoyalPay 创建订单时，签名用上了：
+    # time, nonce_str, partner_code, credential_code
+    # 这 4 项
+    #
+    # 当 RoyalPay 给我们发请求时，传递了 time, nonce_str 和 sign
+    #
+    # 现在思路就很清晰了
+    # 把 RoyalPay 给的 time 和 nonce_str，加上我们的 partner_code 和 credential_code
+    # 运算得到一个 sign
+    # 拿运算出来的 sign，对比传递过来的 sign
+    # 如果一致，就是对的。
+    # 因为只有我们和 RoyalPay，知道 Parter Code 和 Credential Code
+    # 其他人如果试图伪造，sign 会不一致
+    # ====
+    
+    royalpay_partner_code = 'XXXX'
+    royalpay_credential_code = 'AAAAI2M5hVSbBBBBpncrRZjjvZWEZZZZ'
+    
+    calculate_sign = sign(time, nonce_str, royalpay_partner_code, royalpay_credential_code)
+    if calculate_sign == sign
+      return true
+    else
+      return false 
+    end
+    # 计算并对比，返回是否有效 true | false
+  end
+
+end
